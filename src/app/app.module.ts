306ca04e344cc16fa72379726145d7c6ba590dd4
DruSadeMumba <drusademumba@gmail.com>
2022-04-12 18:47:01 +0300

    Create quote class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
 import { FormsModule } from '@angular/forms';
 import { GoalService } from './goal-service/goal.service';
 import { HttpClientModule } from '@angular/common/http';
+import { QuoteComponent } from './quote-class/quote/quote.component';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { HttpClientModule } from '@angular/common/http';
     StrikethroughDirective,
     DateCountPipe,
     GoalFormComponent,
+    QuoteComponent,
   ],
   imports: [
     BrowserModule,
